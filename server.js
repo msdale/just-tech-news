54b55eb3a7a7d0714ada3a7dd20ac550be4c9684
Mark Dale <msdaledad@gmail.com>
2022-01-12 15:02:05 -0500

    completed 13.3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const cliArgs = process.argv.slice(2);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+if (cliArgs[0] && cliArgs[0].toLowerCase() === 'startover') {
+  console.log(`
+  *************************************************
+  *** STARTING OVER...ALL EXISTING DATA IS LOST ***
+  *************************************************
+  `);
+  sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+} else {
+  console.log("*** STARTING UP ***");
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+}
+
