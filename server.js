537f5d52e2fc08a0dd058e75329af645f2fd9ada
Mark Dale <msdaledad@gmail.com>
2022-01-14 10:56:20 -0500

    reverted to original sync command

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-const cliArgs = process.argv.slice(2);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,18 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-if (cliArgs[0] && cliArgs[0].toLowerCase() === 'startover') {
-  console.log(`
-  *************************************************
-  *** STARTING OVER...ALL EXISTING DATA IS LOST ***
-  *************************************************
-  `);
-  sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
-} else {
-  console.log("*** STARTING UP ***");
-  sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
-}
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
