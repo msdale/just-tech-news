debe63de7bfc9b7777a2007f4f5f478bb0a65661
Mark Dale <msdaledad@gmail.com>
2022-01-19 14:34:14 -0500

    updated to 14.2.zip

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -65,7 +65,7 @@ router.post('/', (req, res) => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
-
+  
         res.json(dbUserData);
       });
     })
@@ -95,11 +95,10 @@ router.post('/login', (req, res) => {
     }
 
     req.session.save(() => {
-      // declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
-
+  
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
